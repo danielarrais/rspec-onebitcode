d87ad0235df63dabfd8c485da36c13c3475936bd
GitLab <root@localhost>
2019-08-07 22:51:51 +0200

    Projeto básico de Rails com RSpec

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given('Lucy is located 15m from Sean') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('Sean shouts {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('Lucy hears Sean’s message') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
