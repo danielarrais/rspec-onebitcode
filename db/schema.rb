521644505b80c71bd4f13233ebfd7e864a7df786
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-09-20 22:08:01 -0300

    Testando o Model
    (cherry picked from commit ee6e019557d941899e4db896c7f7372b2aec4235)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_134151) do
+ActiveRecord::Schema.define(version: 2020_09_20_233441) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "nickname"
+    t.integer "kind"
+    t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
