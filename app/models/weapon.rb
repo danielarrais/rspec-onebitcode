f829f112fbf8ac7e6233cffc853633f115d2b4f8
Daniel Arrais <danielarraiscarvalho@gmail.com>
2020-09-26 18:35:12 -0300

    Desafio do herói #3 - Finaliza o desafio

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,9 +1,21 @@
 class Weapon < ApplicationRecord
+
+  after_initialize :init
+  validates_presence_of :name, :description
+
   def current_power
     power_base + ((level - 1) * power_step)
   end
 
   def title
     "#{name} ##{level}"
+  end
+
+  private
+
+  def init
+    self.level ||= 1
+    self.power_base ||= 1000
+    self.power_step ||= 100
   end
 end
--- spec/requests/weapons_request_spec.rb
+++ spec/requests/weapons_request_spec.rb
@@ -3,30 +3,60 @@ require 'rails_helper'
 RSpec.describe "Weapons", type: :request do
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/weapons/index"
-      expect(response).to have_http_status(:success)
+    it "Whether the name, current_power, titles and links is being displayed" do
+      weapons = create_list(:weapon, 5)
+      get weapons_path
+
+      weapons.each do |weapon|
+        expect(response.body).to include(weapon.name)
+        expect(response.body).to include(weapon.current_power.to_s)
+        expect(response.body).to include(weapon.title)
+        expect(response.body).to include(weapon_path(weapon))
+      end
     end
   end
 
   describe "GET /create" do
-    it "returns http success" do
-      get "/weapons/create"
-      expect(response).to have_http_status(:success)
+    context 'when it has valid parameters' do
+      it 'Creates the weapon with correct attributes' do
+        weapon_attributes = FactoryBot.attributes_for(:weapon)
+        post weapons_path, params: { weapon: weapon_attributes }
+
+        expect(Weapon.last).to have_attributes(weapon_attributes)
+      end
+    end
+
+    context 'when it has no valid parameters' do
+      it 'Does not create weapon' do
+        expect {
+          post weapons_path, params: { weapon: { name: '', description: '', power_base: '', power_step: '', level: '' } }
+        }.to_not change(Weapon, :count)
+      end
     end
   end
 
   describe "GET /delete" do
-    it "returns http success" do
-      get "/weapons/delete"
-      expect(response).to have_http_status(:success)
+    it "Delete by passing the correct id" do
+      weapon = create(:weapon)
+      delete weapon_path(weapon)
+
+      expect(Weapon.find_by_id(weapon.id)).to be_nil
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/weapons/show"
-      expect(response).to have_http_status(:success)
+    it "Whether all attributes have been displayed on the screen" do
+      weapon = create(:weapon)
+
+      get weapon_path(weapon)
+
+      expect(response.body).to include(weapon.name)
+      expect(response.body).to include(weapon.description)
+      expect(response.body).to include(weapon.power_base.to_s)
+      expect(response.body).to include(weapon.power_step.to_s)
+      expect(response.body).to include(weapon.current_power.to_s)
+      expect(response.body).to include(weapon.level.to_s)
+      expect(response.body).to include(weapon.title)
     end
   end
 
